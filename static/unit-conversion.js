359c9497c8d68ba92bdc124bd5a1adb20d2def01
leila <leeleiart@gmail.com>
2023-08-01 18:15:56 +0800

    2023/8/1

--- static/unit-conversion.js
+++ static/unit-conversion.js
@@ -1,13 +1,16 @@
 function conversionSize(sizeInBytes){
-    if( sizeInBytes==='error')
-        sizeInBytes=0;
+    if( sizeInBytes==='error') {
+        sizeInBytes = -1;
+        return sizeInBytes.toFixed(2) + 'GB';
+    }
     sizeInBytes=parseInt(sizeInBytes);
     const sizeInGB = sizeInBytes / (1024 * 1024 * 1024);
     return sizeInGB.toFixed(2) + 'GB';
 }
 
-const sizeTds = document.querySelector('[id$="-size"]');
+const sizeTds = document.querySelectorAll('[id$="-size"]');
 sizeTds.forEach(td =>{
     const sizeInBytes = td.textContent;
     td.textContent = conversionSize(sizeInBytes);
+    console.log(td.textContent);
 })
