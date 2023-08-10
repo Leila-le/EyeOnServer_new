4ff87b428bd1dd6ce6d3a7ce13a01c078e04d98e
leila <leeleiart@gmail.com>
2023-08-10 18:03:03 +0800

    2023/8/10

--- eye_on_server/views/data_process.py
+++ eye_on_server/views/data_process.py
@@ -16,7 +16,7 @@ def now_time():
 
 
 json_folder = '/home/leila/djangoProject/EyeOnServer/datas'
-merged_data = []
+merged_data = {}
 
 
 def merge_json_files(folder_path):
@@ -29,83 +29,84 @@ def merge_json_files(folder_path):
             with open(file_path, 'r') as f:
                 file_content = f.read()
                 data = json.loads(file_content)
-                merged_data.extend(data)
+                merged_data.update(data)
         # 如果是子文件夹,则递归
         elif os.path.isdir(file_path):
             merge_json_files(file_path)
 
 
+
 @csrf_exempt
 def data_to_model(request):
-    if request == 'POST':
+    if request.method == 'POST':
         merge_json_files(json_folder)
+        print('merged:', merged_data)
         try:
-            files_path_list = '../static/merger.json'
-            with open(files_path_list, 'w') as f:
+            files_path = './static/merger.json'
+            with open(files_path, 'w', encoding='utf-8') as f:
                 json.dump(merged_data, f)
                 print('JSON文件合并完成.')
-        except:
-            print('合并失败.')
+        except Exception as e:
+            print('合并失败:',e)
         else:
-            for path in files_path_list:
-                with open(path, 'r') as file:
-                    file_content_str = file.read()
-                    data = json.loads(file_content_str)
-                    name = data.get('name')
-                    license_ = data.get('license_name')
-                    # cpu使用情况
-                    cpu_data = data.get('cpu', {})
-                    cpu_guest = cpu_data.get('guest')
-                    cpu_guest_nice = cpu_data.get('guest_nice')
-                    cpu_idle = cpu_data.get('idle')
-                    cpu_iowait = cpu_data.get('iowait')
-                    cpu_nice = cpu_data.get('nice')
-                    cpu_percent = cpu_data.get('percent')
-                    cpu_softirq = cpu_data.get('softirq')
-                    cpu_steal = cpu_data.get('steal')
-                    cpu_system = cpu_data.get('system')
-                    cpu_total_active = cpu_data.get('total_active')
-                    cpu_total_idle = cpu_data.get('total_idle')
-                    cpu_user = cpu_data.get('user')
-                    cpu_count = cpu_data.get('count')
-                    # 磁盘使用情况
-                    disk_data = data.get('disk', {})
-                    disk_free = disk_data.get('free')
-                    disk_mount_point = disk_data.get('mount_point')
-                    disk_total = disk_data.get('total')
-                    disk_used = disk_data.get('used')
-                    disk_percent = disk_used / disk_total
-                    # 内存使用情况
-                    memory_data = data.get('memory', {})
-                    memory_free_physics = memory_data.get('free_physics')
-                    memory_free_swap = memory_data.get('free_swap')
-                    memory_processes = memory_data.get('processes')
-                    memory_total_physics = memory_data.get('total_physics')
-                    memory_total_swap = memory_data.get('total_swap')
-                    memory_uptime = memory_data.get('uptime')
-                    memory_used_physics = memory_data.get('used_physics')
-                    memory_used_swap = memory_data.get('used_swap')
-                    memory_ava = memory_total_physics - memory_used_physics
-                    memory_percent = memory_used_physics / memory_total_physics
-                    time_ = now_time()
-                    SeverInfo.objects.create(name=name, license_name=license_, time=time_, cpu_guest=cpu_guest,
-                                             cpu_guest_nice=cpu_guest_nice,
-                                             cpu_idle=cpu_idle, cpu_iowait=cpu_iowait, cpu_nice=cpu_nice,
-                                             cpu_percent=cpu_percent,
-                                             cpu_softirq=cpu_softirq, cpu_steal=cpu_steal, cpu_system=cpu_system,
-                                             cpu_total_active=cpu_total_active,
-                                             cpu_total_idle=cpu_total_idle, cpu_user=cpu_user, cpu_count=cpu_count,
-                                             disk_free=disk_free, disk_mount_point=disk_mount_point,
-                                             disk_total=disk_total,
-                                             disk_used=disk_used, disk_percent=disk_percent,
-                                             memory_free_physics=memory_free_physics, memory_free_swap=memory_free_swap,
-                                             memory_processes=memory_processes,
-                                             memory_total_physics=memory_total_physics,
-                                             memory_total_swap=memory_total_swap,
-                                             memory_uptime=memory_uptime,
-                                             memory_used_physics=memory_used_physics, memory_used_swap=memory_used_swap,
-                                             memory_ava=memory_ava,
-                                             memory_percent=memory_percent,
-                                             )
+            with open(files_path, 'r') as file:
+                file_content_str = file.read()
+                data = json.loads(file_content_str)
+                name = data.get('name')
+                license_ = data.get('license_name')
+                # cpu使用情况
+                cpu_data = data.get('cpu', {})
+                cpu_guest = cpu_data.get('guest')
+                cpu_guest_nice = cpu_data.get('guest_nice')
+                cpu_idle = cpu_data.get('idle')
+                cpu_iowait = cpu_data.get('iowait')
+                cpu_nice = cpu_data.get('nice')
+                cpu_percent = cpu_data.get('percent')
+                cpu_softirq = cpu_data.get('softirq')
+                cpu_steal = cpu_data.get('steal')
+                cpu_system = cpu_data.get('system')
+                cpu_total_active = cpu_data.get('total_active')
+                cpu_total_idle = cpu_data.get('total_idle')
+                cpu_user = cpu_data.get('user')
+                cpu_count = cpu_data.get('count')
+                # 磁盘使用情况
+                disk_data = data.get('disk', {})
+                disk_free = disk_data.get('free')
+                disk_mount_point = disk_data.get('mount_point')
+                disk_total = disk_data.get('total')
+                disk_used = disk_data.get('used')
+                disk_percent = disk_used / disk_total
+                # 内存使用情况
+                memory_data = data.get('memory', {})
+                memory_free_physics = memory_data.get('free_physics')
+                memory_free_swap = memory_data.get('free_swap')
+                memory_processes = memory_data.get('processes')
+                memory_total_physics = memory_data.get('total_physics')
+                memory_total_swap = memory_data.get('total_swap')
+                memory_uptime = memory_data.get('uptime')
+                memory_used_physics = memory_data.get('used_physics')
+                memory_used_swap = memory_data.get('used_swap')
+                memory_ava = memory_total_physics - memory_used_physics
+                memory_percent = memory_used_physics / memory_total_physics
+                time_ = now_time()
+                SeverInfo.objects.create(name=name, license_name=license_, time=time_, cpu_guest=cpu_guest,
+                                         cpu_guest_nice=cpu_guest_nice,
+                                         cpu_idle=cpu_idle, cpu_iowait=cpu_iowait, cpu_nice=cpu_nice,
+                                         cpu_percent=cpu_percent,
+                                         cpu_softirq=cpu_softirq, cpu_steal=cpu_steal, cpu_system=cpu_system,
+                                         cpu_total_active=cpu_total_active,
+                                         cpu_total_idle=cpu_total_idle, cpu_user=cpu_user, cpu_count=cpu_count,
+                                         disk_free=disk_free, disk_mount_point=disk_mount_point,
+                                         disk_total=disk_total,
+                                         disk_used=disk_used, disk_percent=disk_percent,
+                                         memory_free_physics=memory_free_physics, memory_free_swap=memory_free_swap,
+                                         memory_processes=memory_processes,
+                                         memory_total_physics=memory_total_physics,
+                                         memory_total_swap=memory_total_swap,
+                                         memory_uptime=memory_uptime,
+                                         memory_used_physics=memory_used_physics, memory_used_swap=memory_used_swap,
+                                         memory_ava=memory_ava,
+                                         memory_percent=memory_percent,
+                                         )
 
         return HttpResponse("ok")
