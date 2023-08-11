ef489e1702bbfe6957f3efc8189466dccf38d241
leila <leeleiart@gmail.com>
2023-08-11 17:23:29 +0800

    2023/8/11

--- eye_on_server/views/views.py
+++ eye_on_server/views/views.py
@@ -8,26 +8,25 @@ from .chart import Chart
 
 
 # Create your views here.
-def cpu_percent_line(request, data):
-    chart = Chart()
-    cpu_data_line = chart.line_chart('cpu_avg', 'cpu平均使用率', timezone.now(), SeverInfo.cpu_percent)
-    print('cpu_data_line', cpu_data_line)
-    return render(request, 'monitor/cpu_m.html', locals())
-
-
-def disk_percent_line(request, data):
-    chart = Chart()
-    disk_data_line = chart.line_chart('disk_avg', 'cpu平均使用率', timezone.now(), SeverInfo.disk_percent)
-
-    return render(request, 'monitor/disk_m.html', locals())
-
-
-def memory_percent_line(request, data):
-    chart = Chart()
-    memory_data_line = chart.line_chart('memory_avg', 'cpu平均使用率', timezone.now(), SeverInfo.memory_percent)
-
-    return render(request, 'monitor/memory_m.html', locals())
-
+# def cpu_percent_line(request, data):
+#     chart = Chart()
+#     cpu_data_line = chart.line_chart('cpu_avg', 'cpu平均使用率', timezone.now(), SeverInfo.cpu_percent)
+#     print('cpu_data_line', cpu_data_line)
+#     return render(request, 'monitor/cpu_m.html', locals())
+#
+#
+# def disk_percent_line(request, data):
+#     chart = Chart()
+#     disk_data_line = chart.line_chart('disk_avg', 'cpu平均使用率', timezone.now(), SeverInfo.disk_percent)
+#
+#     return render(request, 'monitor/disk_m.html', locals())
+#
+#
+# def memory_percent_line(request, data):
+#     chart = Chart()
+#     memory_data_line = chart.line_chart('memory_avg', 'cpu平均使用率', timezone.now(), SeverInfo.memory_percent)
+#
+#     return render(request, 'monitor/memory_m.html', locals())
 
 def server(request):
     # 获取模型对象列表
@@ -41,7 +40,7 @@ def server(request):
                              memory=item.memory_percent,
                              cpu=item.cpu_percent,
                              disk=item.disk_percent,
-                             joinTime=join_time_str )
+                             joinTime=join_time_str)
         data.append(overview_data)
     merged_data = {'code': 0, 'data': data}
     file_path = './static/merger.json'
@@ -53,7 +52,21 @@ def server(request):
     return render(request, 'ServerList.html')
 
 
+def draw_line():
+    chart = Chart()
+    server_info_list = SeverInfo.objects.filter(license_name='泉州熊猫科技有限公司')
+    result_cpu = server_info_list.values_list('cpu_percent', 'time')
+    result_memory = server_info_list.values_list('memory_percent', 'time')
+    result_disk = server_info_list.values_list('disk_percent', 'time')
+
+    cpu_data_line = chart.line_chart('cpu_avg', 'cpu平均使用率', result_cpu)
+    disk_data_line = chart.line_chart('disk_avg', 'cpu平均使用率', result_memory)
+    memory_data_line = chart.line_chart('memory_avg', 'cpu平均使用率', result_disk)
+
+
 def home(request):
     infos = SeverInfo.objects.all().order_by('time')
     context = {'infos': infos}
+
+    draw_line()
     return render(request, "base.html", context=context)
